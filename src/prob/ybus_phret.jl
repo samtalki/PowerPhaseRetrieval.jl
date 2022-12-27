9b1f289a16b07becd132233c4aab8520ebde42d1
samtalki <saimouer@gmail.com>
2022-12-27 11:13:38 -0500

    ybus phret

--- src/prob/ybus_phret.jl
+++ src/prob/ybus_phret.jl
@@ -9,5 +9,8 @@ function YbusPhaseRetrievalModel(net::Dict{String,Any})
     compute_ac_pf!(net)
     Y = calc_basic_admittance_matrix(net)
     s = calc_basic_bus_injection(net)
+    v = calc_basic_bus_injection(net)
+    vm,va = abs.(v),angle.(v)
+    
 end
 
