e52336289df7a3cfd38b4e9508a0002477f8e9cb
samtalki <saimouer@gmail.com>
2023-04-06 13:50:53 -0400

    Hourly sequential phret, errs very small.

--- src/prob/nr_ph_retrieve.jl
+++ src/prob/nr_ph_retrieve.jl
@@ -36,7 +36,104 @@ import Ipopt
 #Set the seed
 Random.seed!(2023)
 
+function est_bus_voltage_phase(va_nom,∂pv,∂qv,vm_obs,f_x_obs,q_nom,p_nom,∂pθ_true,∂qθ_true)
+    n_bus = length(va_nom)
+    #make phase retrieval model
+    model = Model(Ipopt.Optimizer)
+    #set_silent(model)
 
+    #----- Variables and expressions
+    #--- Phase VariableS
+    @variable(model,θ[1:n_bus])
+
+    #--- Phase jacobian variables
+    @variable(model,∂pθ[1:n_bus,1:n_bus])
+    @variable(model,∂qθ[1:n_bus,1:n_bus])
+
+    #---- Expressions
+    #- Grid state with unknown phase angles.
+    x = [θ; vm_obs]
+    #- Jacobian matrix
+    J = [
+        ∂pθ ∂pv;
+        ∂qθ ∂qv
+    ]
+    #- Residual expression
+    @variable(model,resid[1:2*n_bus])
+    @constraint(model,resid .== J*x .- f_x_obs)
+
+    #----- Constraints
+    #Jacobian physics constraints
+    for i =1:n_bus
+        @constraint(model,
+            ∂pθ[i,i] == vm_obs[i]*∂qv[i,i] - 2*q_nom[i]
+        )
+        @constraint(model,
+            ∂qθ[i,i] == -vm_obs[i]*∂pv[i,i] + 2*p_nom[i]
+        )
+        @constraint(model,
+            [k=1:n_bus; k!= i],
+            ∂pθ[i,k] == vm_obs[k]*∂qv[i,k]
+        )
+        @constraint(model,
+            [k=1:n_bus; k!=i],
+            ∂qθ[i,k] == -vm_obs[k]*∂pv[i,k]
+    )
+    end
+
+    #----- Objective - min sum of square errors
+    @objective(model,Min,sum(resid.^2))
+
+    optimize!(model)
+
+    #construct phasor voltages
+    θ_hat = value.(θ)
+    v_rect_hat = vm_obs .* (cos.(θ_hat) + sin.(θ_hat) .* im)
+    v_rect_true = vm_obs .* (cos.(va_nom) + sin.(va_nom) .* im)
+
+    #------- Construct current phasors
+    L_rect_true = conj.((p_nom .+ q_nom.*im)./ v_rect_true)
+    L_rect_hat = conj.((p_nom .+ q_nom.*im)./v_rect_hat)
+    
+    #param covariance
+    ∂pθ = value.(∂pθ)
+    ∂qθ = value.(∂qθ)
+    
+    Je = Matrix([
+        ∂pθ ∂pv;
+        ∂qθ ∂qv
+    ])
+    #Construct the standard error:
+    #se = sqrt(sum((va_nom.- θ_hat).^2)/(length(θ_hat)-2))
+    mse = (1/length(resid))*sum(value.(resid).^2)#(1/length(va_nom))*sum((va_nom.- θ_hat).^2)
+    se = sqrt.(diag(inv(Je'*Je))*mse)[1:n_bus]#sqrt.(diag(inv(Jph'*Jph )) * mse)#sqrt.(diag(inv(∂pθ' * ∂pθ + ∂qθ' * ∂qθ))*mse)
+
+    #construct covariance of the error
+    err_cov = diag((va_nom .- θ_hat)*(va_nom .- θ_hat)')
+    
+
+    #th_std = sqrt.(diag((inv(Je'*Je)*Je')*(va_nom*va_nom')*(Je*inv(Je'*Je))))
+    
+    
+
+    #Return the results dict
+    return Dict(
+        "th_hat"=>θ_hat,
+        "th_true"=>va_nom,
+        "v_rect_hat"=>v_rect_hat,
+        "dpth"=>value.(∂pθ),
+        "dqth"=>value.(∂qθ),
+        "th_sq_errs" => (abs.(va_nom .- θ_hat)).^2,
+        "th_rel_err"=> norm(va_nom- θ_hat)/norm(va_nom)*100,
+        "std_err"=>se ,
+        "err_cov"=>err_cov,
+        # "th_std="=>th_std,
+        "dpth_rel_err"=>norm(value.(∂pθ)- ∂pθ_true)/norm(∂pθ_true)*100,
+        "dqth_rel_err"=>norm(value.(∂qθ)- ∂qθ_true)/norm(∂qθ_true)*100,
+        "L_rel_err"=> norm(L_rect_true .- L_rect_hat)./(norm(L_rect_true))*100
+        #"v_rect_rel_err"=> norm(v_rect_nom-v_rect_hat)/norm(v_rect_nom)*100
+    )
+end
 
 """
 Given a basic network data dict, compute AC power flow solution and retrieve the voltage phases.
@@ -378,18 +475,18 @@ end
 
 
 
-"""
-Find the closest rank-R approximate matrix of A
-"""
-function calc_closest_rank_r(A::Matrix,r::Integer)
-    (m,n) = size(A)
-    U,Σ,V = svd(A)
-    for (i,s_i) in enumerate(Σ)
-        if i > r 
-            Σ[i] = 0
-        end
-    end
-    return U * Diagonal(Σ) * V' 
-end
+# """
+# Find the closest rank-R approximate matrix of A
+# """
+# function calc_closest_rank_r(A::Matrix,r::Integer)
+#     (m,n) = size(A)
+#     U,Σ,V = svd(A)
+#     for (i,s_i) in enumerate(Σ)
+#         if i > r 
+#             Σ[i] = 0
+#         end
+#     end
+#     return U * Diagonal(Σ) * V' 
+# end
 
 
--- tests/timeseries_rts_phret.jl
+++ tests/timeseries_rts_phret.jl
@@ -1,17 +1,205 @@
+# create synthetic data for one day at 15 minute granularity driven by smart meter data varying demands. 
+# Then assign random delays within a 15 minute range (e.g. uniform distribution). See the impact of lack of synchronization in the data stream. 
+# My strong believe is that the method will be robust enough to generate the matrices and phase retrieval that have small error. 
+
 include("../src/PowerPhaseRetrieval.jl")
 import .PowerPhaseRetrieval as PPR
-using PowerModels,LinearAlgebra,Plots,DataFrames,CSV
+using PowerModels,LinearAlgebra,Plots
+using DataFrames,CSV
+using Distributions,Random
 
-# load case and run power flow
-net = make_basic_network(parse_file("data/networks/case_RTS_GMLC.m"))
-compute_ac_pf!(net)
+"""
+Generate a vector of time series load datas for the network.
+"""
+function generate_updates(net::Dict,mw_data::DataFrame,mvar_data::DataFrame)
+    m_samples,n_buses = size(mw_data)
+    baseMVA = net["baseMVA"]
+    @assert n_buses == length(net["bus"])
+    @assert haskey(net,"basic_network") #must be a basic network
+    # setup the load updates
+    load_updates = [] # list of network load updates
+    load_dict = net["load"] #current load dictionary
+    for t=1:m_samples #for every time series sample
+        load_dict_t = Dict{String,Any}()
+        for (load_id,load_entry) in load_dict
+            #bus_id = string(load_entry["load_bus"])
+            _,bus_id = load_entry["source_id"]
+            bus_id = string(bus_id)
+            pd_t, qd_t = mw_data[t,bus_id]/baseMVA, mvar_data[t,bus_id]/baseMVA
+            load_dict_t[load_id] = Dict("pd"=>pd_t,"qd"=>qd_t)
+        end
+        push!(load_updates,Dict("load"=>load_dict_t))
+    end
+    return load_updates
+end
 
-# load the averaged timeseries data
-mvar_data = DataFrame(CSV.File("data/RTS-GMLC-timeseries/nodal-dayahead/nodal_mvar_data.csv"))
-mw_data = DataFrame(CSV.File("data/RTS-GMLC-timeseries/nodal-dayahead/nodal_mw_data.csv"))
+"""
+apply updates and solve power flows to generate voltage magnitudes, active powers, and reactive powers, and jacobian structs
+"""
+function generate_phase_retrieval_data(net::Dict,load_updates::Vector;sel_bus_types=[1])
+    @assert haskey(net,"basic_network") #must be a basic network
+    # select the bus indeces
+    sel_bus_idx = PPR.calc_bus_idx_of_type(net,sel_bus_types)
+    # setup the data containers
+    vms,vas,ps,qs,jacs = [],[],[],[],[]
+    for lu_t in load_updates
 
-# generate random delay
-function generate_delay()
+        # update the network
+        update_data!(net,lu_t)
+        compute_ac_pf!(net)
+        
+        # calculate the data
+        vm_t = abs.(calc_basic_bus_voltage(net))[sel_bus_idx]
+        va_t = angle.(calc_basic_bus_voltage(net))[sel_bus_idx]
+        inj = calc_basic_bus_injection(net)[sel_bus_idx]
+        p_t,q_t = real.(inj),imag.(inj)
+        jac_t = PPR.calc_jacobian_matrix(net,sel_bus_types)
+
+        # append the data
+        push!(vms,vm_t)
+        push!(vas,va_t)
+        push!(ps,p_t)
+        push!(qs,q_t)
+        push!(jacs,jac_t)
+
+    end
+
+    return vms,vas,ps,qs,jacs
+end
+
+"""
+Generates the real time va data using the 5 minute data
+"""
+function generate_realtime_va_data(net::Dict,load_updates::Vector;sel_bus_types=[1])
+    @assert haskey(net,"basic_network") #must be a basic network
+    # select the bus indeces
+    sel_bus_idx = PPR.calc_bus_idx_of_type(net,sel_bus_types)
+    # setup the data containers
+    vas = []
+    for lu_t in load_updates
+        # update the network
+        update_data!(net,lu_t)
+        compute_ac_pf!(net)
+        va_t = angle.(calc_basic_bus_voltage(net))[sel_bus_idx]
+        push!(vas,va_t)
+    end
+    return vas
+end
+
+"""
+Generates random delays for real-time data
+"""
+function generate_delays()
 
 
 end
+
+
+"""
+Given vectors of voltage magnitudes, active powers, reactive powers, and jacobian models
+solve the jac recovery phase retrieval problem sequentially
+"""
+function test_sequential_est_bus_voltage_phase(vms,vas,ps,qs,jacs;D_f_x=nothing,D_vm=nothing)
+    phret_results = []
+    n_buses = length(vms[1])
+    for (vm_t,va_t,p_t,q_t,jac_t) in zip(vms,vas,ps,qs,jacs)
+        # generate the noise at this sample
+        ϵ_f_x = D_f_x !== nothing ? rand(D_f_x,2*n_buses) : 0
+        ϵ_vm = D_vm !== nothing ? rand(D_vm,n_buses) : 0
+        # solve the phase retrieval problem
+        est_results = PPR.est_bus_voltage_phase( #va_nom,∂pv,∂qv,vm_obs,f_x_obs,q_nom,p_nom,∂pθ_true,∂qθ_true
+            va_t,
+            Matrix(jac_t.pv), #∂pv
+            Matrix(jac_t.qv), #∂qv
+            vm_t .+ ϵ_vm, #vm_obs
+            Matrix(jac_t.matrix)*[va_t;vm_t] .+ ϵ_f_x, #f_x_obs
+            q_t, #q_nom
+            p_t, #p_nom
+            Matrix(jac_t.pth), #∂pth
+            Matrix(jac_t.qth) #∂qth
+            )
+        push!(phret_results,est_results)
+    end
+    return phret_results
+end
+
+"""
+Calculates the root mean squared error of the estimated voltage phase angles estimated in each hourly interval
+with the true phase angles using the 5minute data.
+"""
+function calc_hourly_rms_error(hourly_phret_results::Vector,realtime_phase_angles::Vector)
+    @assert length(realtime_phase_angles) == 12*length(hourly_phret_results)
+    hourly_rms_error = []
+    for i=1:length(hourly_phret_results)
+        # calculate the hourly rms error
+        hourly_rms_error_t = 0
+        est_va_t = hourly_phret_results[i]
+        for j=1:12
+            t = (i-1)*12 + j
+            true_va_t = realtime_phase_angles[t]
+            hourly_rms_error_t += sum((est_va_t - true_va_t).^2)
+        end
+        hourly_rms_error_t = sqrt(hourly_rms_error_t/12)
+        push!(hourly_rms_error,hourly_rms_error_t)
+    end
+    return hourly_rms_error
+end
+
+
+# load case and run power flow
+net = make_basic_network(parse_file("data/networks/case_RTS_GMLC.m")) #basic network
+pq_buses = PPR.calc_bus_idx_of_type(net,[1])
+compute_ac_pf!(net)
+p0,q0 = real.(calc_basic_bus_injection(net)[pq_buses]),imag.(calc_basic_bus_injection(net)[pq_buses])
+
+# -----------------
+# generate m_samples of timeseries data 
+# ----------------
+m_hours = 48 # number of hours to generate
+
+
+#---------------- generate the hourly power flow result data
+#--- load the averaged timeseries data
+hourly_mvar_data = DataFrame(CSV.File("data/RTS-GMLC-timeseries/nodal-dayahead/nodal_mvar_data.csv"))
+hourly_mw_data = DataFrame(CSV.File("data/RTS-GMLC-timeseries/nodal-dayahead/nodal_mw_data.csv"))
+
+# remove time index column
+time_idx,hourly_mw_data = hourly_mw_data[1:m_hours,1],hourly_mw_data[1:m_hours,2:end]
+time_idx,hourly_mvar_data = hourly_mvar_data[1:m_hours,1],hourly_mvar_data[1:m_hours,2:end]
+
+#--- generate the hourly load updates and grid states
+hourly_load_updates = generate_updates(net,hourly_mw_data,hourly_mvar_data)
+vms,vas,ps,qs,jacs = generate_phase_retrieval_data(net,hourly_load_updates)
+
+#---------------- generate real time power flow angle data
+# #--- load the real time data
+# m_5mins = 12*m_hours
+# inst_mvar_data = DataFrame(CSV.File("data/RTS-GMLC-timeseries/nodal-dayahead/nodal_mvar_data.csv"))
+# inst_mw_data = DataFrame(CSV.File("data/RTS-GMLC-timeseries/nodal-dayahead/nodal_mw_data.csv"))
+
+# # remove time index column
+# time_idx,inst_mw_data = inst_mw_data[1:m_5mins,1],inst_mw_data[1:m_5mins,2:end]
+# time_idx,inst_mvar_data = inst_mvar_data[1:m_5mins,1],inst_mvar_data[1:m_5mins,2:end]
+
+# #--- generate the real time va data
+# realtime_load_updates = generate_updates(net,inst_mw_data,inst_mvar_data)
+# realtime_vas = generate_realtime_va_data(net,realtime_load_updates)
+
+
+# # test the phase retrieval problem sequentially using the averaged data
+hourly_phret_results = test_sequential_est_bus_voltage_phase(vms,vas,ps,qs,jacs)
+    #D_f_x=Normal(0,1e-3),D_vm=Normal(0,1e-4))
+
+# calculate the rms error
+est_hourly_vas = [result["th_hat"] for result in hourly_phret_results]
+rel_err_hourly_vas = [result["th_rel_err"] for result in hourly_phret_results]
+
+
+# calculate the 
+#hourly_rms_error = calc_hourly_rms_error(est_hourly_vas,realtime_vas)
+
+
+#--- test the jacobian symmetry at the first time step
+dpv1,dqv1 = Matrix(jacs[1].pv),Matrix(jacs[1].qv)
+dpth1,dqth1 = Matrix(jacs[1].pth),Matrix(jacs[1].qth)
+dpξ,dqξ = calc_augmented_angle_jacobians(vms[1],ps[1],qs[1],dpv1,dqv1)
