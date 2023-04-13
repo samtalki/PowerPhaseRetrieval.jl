3eb13cd721d99a13b7386d47d63afbfdec46c4cf
samtalki <saimouer@gmail.com>
2023-04-13 10:37:11 -0400

    unbalanced topology-based jacobians works

--- tests/unbalanced.jl
+++ tests/unbalanced.jl
@@ -2,6 +2,7 @@ using Plots,LaTeXStrings
 using PowerModelsDistribution
 using LinearAlgebra
 using ForwardDiff
+using Ipopt
 
 # Load the data -- case 3 unbalanced
 eng = parse_file("data/unbalanced/case3_unbalanced.dss")
@@ -15,11 +16,100 @@ result = solve_mc_pf(math,ACPUPowerModel,Ipopt.Optimizer)
 K = length(math["bus"]) # number of buses
 Y = zeros(ComplexF64,3*K,3*K)
 
-for (branch_idx,branch) in math["branch"]
 
+#--------------------- off diagonal admittances ---------------------
+for (branch_idx,branch) in math["branch"]
     fr_bus, to_bus = branch["f_bus"], branch["t_bus"]    
     fr_idx, to_idx = math["bus"][string(fr_bus)]["index"], math["bus"][string(to_bus)]["index"]
     print(fr_idx,to_idx)
     G,B = calc_branch_y(branch)
-    Y[3*fr_idx-2:3*fr_idx,3*fr_idx-2:3*fr_idx] -= G + im.*B
+    Y[3*fr_idx-2:3*fr_idx,3*to_idx-2:3*to_idx] -= G + im.*B
+    Y[3*to_idx-2:3*to_idx,3*fr_idx-2:3*fr_idx] -= G + im.*B
 end
+
+# --------------------- diagonal admittances ---------------------
+for (bus_idx,bus) in math["bus"]
+    i = bus["index"]
+    Y[3*i-2:3*i,3*i-2:3*i] = zeros(ComplexF64,3,3)
+    for j=1:K # for each column block
+        if j==i
+            continue
+        else
+            Y[3*i-2:3*i,3*i-2:3*i] -= Y[3*i-2:3*i,3*j-2:3*j] # subtract the off-diagonal elements to get the diagonal elements
+        end
+    end
+end
+
+@assert all(Y .≈ conj(Y')) "Y must be symmetric"
+
+
+# --------------------- Get the power flow solution data ---------------------
+vm,va = zeros(Float64,3*K),zeros(Float64,3*K) #voltage magnitudes and angles (3)
+p,q = zeros(Float64,3*K),zeros(Float64,3*K) #active and reactive power net injections
+
+#--- voltage magnitude and phase angles
+for (bus_id,bus_sol) in result["solution"]["bus"]
+    i = math["bus"][bus_id]["index"]
+    vm[3*i-2:3*i] .= bus_sol["vm"]
+    va[3*i-2:3*i] .= bus_sol["va"]
+end
+
+#--- active and reactive power net injections
+
+# loads
+for (load_id,load_sol) in result["solution"]["load"]
+    i = math["load"][load_id]["load_bus"] 
+    pd = load_sol["pd_bus"]
+    qd = load_sol["qd_bus"]
+    p[3*i-2:3*i] .-= pd
+    q[3*i-2:3*i] .-= qd
+end
+
+# generators
+for (gen_id,gen_sol) in result["solution"]["gen"]
+    i = math["gen"][gen_id]["gen_bus"]
+    pg = gen_sol["pg_bus"]
+    qg = gen_sol["qg_bus"]
+    p[3*i-2:3*i] .+= pg
+    q[3*i-2:3*i] .+= qg
+end
+
+#---- voltage phasors and complex power injections
+vph = vm .* cis.(va) # voltage phasors
+s = p + im.*q # complex power injections
+
+# --------------------- Form the Jacobian matrices ---------------------
+#--- Jacobian of the power flow equations w.r.t. voltage magnitudes and angles,
+# "A survey of relaxations and approximations of the power flow equations", Molzahn and Hiskens (2013)
+function dsθ(vm,va)
+    vph = vm .* cis.(va)
+    ∂sθ = im.*(
+        Diagonal(vph)*(
+            Diagonal( conj.(Y)*conj.(vph) ) - conj.(Y)*Diagonal( conj.(vph) )
+        )
+    )
+    return ∂sθ
+end
+
+function dsvm(vm,va)
+    vph = vm .* cis.(va)
+    ∂svm = Diagonal(vph)*(
+        Diagonal( conj.(Y)*conj.(vph) ) + conj.(Y)*Diagonal( conj.(vph) )
+    )*Diagonal(1 ./ vm)
+    return ∂svm 
+end
+
+#----- real and imaginary parts of the Jacobian matrices
+dpθ(vm,va)= real.(dsθ(vm,va))
+dqθ(vm,va) = imag.(dsθ(vm,va))
+dpvm(vm,va) = real.(dsvm(vm,va))
+dqvm(vm,va) = imag.(dsvm(vm,va))
+
+#---- test the symmetry hypothesis of the Jacobian matrices
+
+dpξ(vm,va,q) = Diagonal(vm)*dqvm(vm,va) - 2*Diagonal(q) # ∂p/∂ξ
+dqξ(vm,va,p) = -Diagonal(vm)*dpvm(vm,va) + 2*Diagonal(p) # ∂q/∂ξ
+
+#----- test the symmetry hypothesis of the Jacobian matrices
+err_dpθ = norm(dpθ(vm,va) - dpξ(vm,va,q))/norm(dpθ(vm,va))
+err_dqθ = norm(dqθ(vm,va) - dqξ(vm,va,p))/norm(dqθ(vm,va))
