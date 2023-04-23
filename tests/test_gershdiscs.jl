00555b045fb45f6c80565af77ae0e458f8c00e0f
samtalki <saimouer@gmail.com>
2023-04-23 10:10:06 -0400

    mf noise levels

--- tests/test_gershdiscs.jl
+++ tests/test_gershdiscs.jl
@@ -13,10 +13,22 @@ cases = readdir(path)
 
 # Test the Gershgorin disc theorems
 case_results = Dict()
+
+#---- dpdth and dqdth individual recoverability
+dpth_phase_recoverable = Dict()
+dqth_phase_recoverable = Dict()
+pct_dpth_phase_recoverable = Dict()
+pct_dqth_phase_recoverable = Dict()
+pct_dpth_strong_phase_recoverable = Dict()
+pct_dqth_strong_phase_recoverable = Dict()
+
+#---- total phase recoverability
 phase_recoverable = Dict()
 strong_phase_recoverable = Dict()
 pct_phase_recoverable = Dict()
 pct_strong_phase_recoverable = Dict()
+
+
 jac_recoverable = Dict()
 pct_jac_recoverable = Dict()
 worst_lhs_radii = Dict()
@@ -45,7 +57,11 @@ for case in cases
     case_results[case]["dpv"] = dpv
     case_results[case]["dqv"] = dqv
 
-    #--- observability ---#
+    #-- dpth and dqth individual recoverability ---#
+    dpth_phase_recoverable[case] = dpdth_observability(net)
+    dqth_phase_recoverable[case] = dqdth_observability(net)
+
+    #--- full observability conditions ---#
     n_pq_bus[case] = size(dpξ)[1]
     worst_radius,ph_rec = dpdth_dqdth_observability(net)
     worst_lhs_radii[case] = worst_radius
@@ -95,4 +111,73 @@ for case in cases
     pct_strong_phase_recoverable[case] = sum(strong_phase_recoverable[case])/length(strong_phase_recoverable[case])
     jac_recoverable[case] = case_results[case]["jac_recoverable"]
 
+    # save the dpth and dqth individual recoverabilities
+    pct_dpth_phase_recoverable[case] = sum(dpth_phase_recoverable[case].observable)/(n_pq_bus[case])
+    pct_dqth_phase_recoverable[case] = sum(dqth_phase_recoverable[case].observable)/(n_pq_bus[case])
+    pct_dpth_strong_phase_recoverable[case] = sum(dpth_phase_recoverable[case].strong_observable)/(n_pq_bus[case])
+    pct_dqth_strong_phase_recoverable[case] = sum(dqth_phase_recoverable[case].strong_observable)/(n_pq_bus[case])
+
 end
+
+pct_block_phase_recoverable = Dict()
+pct_block_strong_phase_recoverable = Dict()
+
+for case in cases
+    pct_block_phase_recoverable[case]= max(pct_dpth_phase_recoverable[case],pct_dqth_phase_recoverable[case])
+    pct_block_strong_phase_recoverable[case] =  max(pct_dpth_strong_phase_recoverable[case],pct_dqth_strong_phase_recoverable[case])
+    
+    dpth_lhs,dpth_rhs_col,dpth_rhs_row = dpth_phase_recoverable[case].lhs,dpth_phase_recoverable[case].rhs_col,dpth_phase_recoverable[case].rhs_row
+    dqth_lhs,dqth_rhs_col,dqth_rhs_row = dqth_phase_recoverable[case].lhs,dqth_phase_recoverable[case].rhs_col,dqth_phase_recoverable[case].rhs_row
+    
+    w_pth = [min(dpth_lhs_i - dpth_rhs_col_i,dpth_lhs_i - dpth_rhs_row_i) for (dpth_lhs_i,dpth_rhs_col_i,dpth_rhs_row_i) in zip(dpth_lhs,dpth_rhs_col,dpth_rhs_row)]
+    w_qth = [min(dqth_lhs_i - dqth_rhs_col_i,dqth_lhs_i - dqth_rhs_row_i) for (dqth_lhs_i,dqth_rhs_col_i,dqth_rhs_row_i) in zip(dqth_lhs,dqth_rhs_col,dqth_rhs_row)]
+    worst_case_radii[case] = maximum(min(w_pth,w_qth))
+
+end
+
+# worst_case_radii = Dict()
+# for case in cases
+#     #--- get the observability struct---#
+#     dpo = dpth_phase_recoverable[case]
+#     dqo = dqth_phase_recoverable[case]
+
+#     #--- get the worst case radii ---#
+#     worst_radii = []
+
+#     dpth_lhs,dpth_rhs_col,dpth_rhs_row = dpth_phase_recoverable[case].lhs,dpth_phase_recoverable[case].rhs_col,dpth_phase_recoverable[case].rhs_row
+#     dqth_lhs,dqth_rhs_col,dqth_rhs_row = dqth_phase_recoverable[case].lhs,dqth_phase_recoverable[case].rhs_col,dqth_phase_recoverable[case].rhs_row
+#     WPTH,WQTH = [],[]
+#     for i=1:length(dpth_lhs)
+#         if dpo.strong_observable[i] 
+#             push!(WPTH,0)
+#         elseif dqo.strong_observable[i]
+#             push!(WQTH,0)
+#         elseif !(dpo.strong_observable[i])
+#             push!(worst_radii,
+#                 min(dpth_rhs_row[i],dpth_rhs_col[i]) - dpth_lhs[i]
+#             )    
+        
+        
+#         if !(dpth_phase_recoverable[case].strong_observable[i])
+#             push!(worst_radii,
+#                 min(dpth_rhs_row[i],dpth_rhs_col[i]) - dpth_lhs[i]
+#             )
+#         elseif !(dqth_phase_recoverable[case].strong_observable[i])
+#             push!(worst_radii,
+#                 min(dqth_rhs_row[i],dqth_rhs_col[i]) - dqth_lhs[i]
+#             )
+#         end
+#         w_pth = min(dpth_rhs_row[i],dpth_rhs_col[i]) - dpth_lhs[i]
+#         w_qth = min(dqth_rhs_row[i],dqth_rhs_col[i]) - dqth_lhs[i]
+#         push!(WPTH,w_pth)
+#         push!(WQTH,w_qth)
+#     end
+#     worst_radii = min.(WPTH,WQTH)
+#     worst_radius = maximum(worst_radii)
+#     worst_case_radii[case] = worst_radius
+# end
+
+
+#--- save the worst case radii ---#
+worst_case_radii = Dict()
+2+2==4
